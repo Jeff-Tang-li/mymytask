97c21ac26cac535b4bfe97494620faa6f1fde513
tangxinli <jeff.li@chintec.net>
2021-01-23 12:18:58 +0800

    Initial commit

---
+++ mytask-service/src/main/java/org/mytask/annotation/TableField.java
@@ -0,0 +1,12 @@
+package org.mytask.annotation;
+
+/**
+ * 数据库字段映射字段属性
+ *
+ * @author Jeff·Tang
+ * @version 1.0
+ * @date 2021/1/22 10:29
+ */
+public @interface TableField {
+    
+}
---
+++ mytask-service/src/main/java/org/mytask/entity/TableEntity.java
@@ -0,0 +1,11 @@
+package org.mytask.entity;
+
+/**
+ * 此为TableField 解析类
+ *
+ * @author Jeff·Tang
+ * @version 1.0
+ * @date 2021/1/23 12:09
+ */
+public class TableEntity {
+}

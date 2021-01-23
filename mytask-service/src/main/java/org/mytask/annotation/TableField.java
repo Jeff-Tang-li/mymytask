b310b1079d2d35bcefda3f7fb7abdb2f77a36730
tangxinli <jeff.li@chintec.net>
2021-01-23 22:44:35 +0800

    author:jeff·tang time:2021年1月23日12:23:53 content:init project

--- mytask-service/src/main/java/org/mytask/annotation/TableField.java
+++ mytask-service/src/main/java/org/mytask/annotation/TableField.java
@@ -1,5 +1,7 @@
 package org.mytask.annotation;
 
+import java.lang.annotation.*;
+
 /**
  * 数据库字段映射字段属性
  *
@@ -7,6 +9,15 @@ package org.mytask.annotation;
  * @version 1.0
  * @date 2021/1/22 10:29
  */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface TableField {
-    
+
+    String id() default "";
+
+    String name() default "";
+
+    String type() default "";
+
 }
--- mytask-service/src/main/java/org/mytask/entity/TableEntity.java
+++ mytask-service/src/main/java/org/mytask/entity/TableEntity.java
@@ -8,4 +8,41 @@ package org.mytask.entity;
  * @date 2021/1/23 12:09
  */
 public class TableEntity {
+    /**
+     * 解析 id
+     */
+    private String id;
+    /**
+     * 字段名
+     */
+    private String name;
+    /**
+     * 字段类型
+     */
+    private String type;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
